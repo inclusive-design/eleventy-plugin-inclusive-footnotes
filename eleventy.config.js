b1dba7a71d4cb4a6eca9998ebbd46466d4cef9e6
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-03-19 16:31:02 -0300

    fix: resolve linting issues, configure husky, add a test (#3)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,24 +1,28 @@
-import inclusiveFootnotesPlugin from "./index.js";
+import inclusiveFootnotesPlugin from './index.js';
 
+/**
+ * @param {object} eleventyConfig The Eleventy configuration object.
+ * @returns {object} Eleventy configuration.
+ */
 export default function eleventy(eleventyConfig) {
 	eleventyConfig.addPlugin(inclusiveFootnotesPlugin, {
 		translations: {
-      en: {
-     		footnote_ref: "Endnote %{id}",
-			footnotes: "Endnotes",
-			backlink: "Back to reference %{ref} for endnote %{footnote}"
-      },
-      de: {
-        footnote_ref: "Endnote %{id}",
-        footnotes: "Endnoten",
-        backlink: "Zurück zur Referenz %{ref} für Endnote %{footnote}"
-      }
-    }
+			en: {
+				footnote_ref: 'Endnote %{id}',
+				footnotes: 'Endnotes',
+				backlink: 'Back to reference %{ref} for endnote %{footnote}',
+			},
+			de: {
+				footnote_ref: 'Endnote %{id}',
+				footnotes: 'Endnoten',
+				backlink: 'Zurück zur Referenz %{ref} für Endnote %{footnote}',
+			},
+		},
 	});
 
 	return {
 		dir: {
-			input: 'fixtures'
-		}
-	}
+			input: 'fixtures',
+		},
+	};
 }
